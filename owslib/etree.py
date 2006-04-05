406c6b7de65c73681a6784a9605be707c59cdad9
seang <seang@b426a367-1105-0410-b9ff-cdf4ab011145>
2006-04-05 03:49:04 +0000

    Update cartography.data.ows to use owslib, implement typenames(), bounds(), and info() for W*S stores (ticket #40)

---
+++ owslib/etree.py
@@ -0,0 +1,34 @@
+# =============================================================================
+# Cartographic Objects for Zope. Copyright (C) 2005 Sean C. Gillies
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# Contact email: sgillies@frii.com
+# =============================================================================
+
+# try to find elementtree or lxml
+try:
+    import elementtree.ElementTree as etree
+    # Monkey Patch adds to the default well known namespaces
+    etree._namespace_map.update({
+        "http://www.w3.org/1999/02/22-rdf-syntax-ns#":  "rdf", 
+        "http://purl.org/rss/1.0/":                     "rss", 
+        "http://purl.org/rss/1.0/modules/taxonomy/":    "taxo", 
+        "http://purl.org/dc/elements/1.1/":             "dc", 
+        "http://purl.org/rss/1.0/modules/syndication/": "syn", 
+        "http://www.w3.org/2003/01/geo/wgs84_pos#":     "geo"})
+except:
+    from lxml import etree
+
