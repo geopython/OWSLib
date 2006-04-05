87b755b45b0e7e33151eaefed42ef53135764b67
seang <seang@b426a367-1105-0410-b9ff-cdf4ab011145>
2006-04-05 03:33:34 +0000

    New location for OWSLib, formerly known as ogclib

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(name          = 'OWSLib',
+      version       = '0.0',
+      description   = 'OGC Web Service utility package',
+      author        = 'Sean Gillies',
+      author_email  = 'sgillies@frii.com',
+      url           = 'http://www.gispython.org',
+      packages      = ['owslib']
+)
+
