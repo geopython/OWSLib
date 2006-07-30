72b1d46fa5e4b09fe54a5d68972982a5862ae969
seang <seang@b426a367-1105-0410-b9ff-cdf4ab011145>
2006-07-30 21:20:12 +0000

    New meta files for owslib

--- owslib/etree.py
+++ owslib/etree.py
@@ -1,5 +1,5 @@
 # =============================================================================
-# Cartographic Objects for Zope. Copyright (C) 2005 Sean C. Gillies
+# OWSLib. Copyright (C) 2005 Sean C. Gillies
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
