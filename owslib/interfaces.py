0775b1b91b73021161e4a1725a2ed1e0f7b3f190
seang <seang@b426a367-1105-0410-b9ff-cdf4ab011145>
2006-10-17 16:49:04 +0000

    Flesh out ows metadata interfaces

--- owslib/interfaces.py
+++ owslib/interfaces.py
@@ -9,8 +9,8 @@ class IWebMapService:
         Online resource URL.
     version : string
         WMS protocol version.
-    capabilities : object
-        Describes the capabilities of the WMS.
+    metadata : object
+        Describes the metadata (formerly called capabilities) of the WMS.
     """
 
     def getcapabilities():
@@ -22,4 +22,95 @@ class IWebMapService:
     def getfeatureinfo(**kw):
         """Make a request to the WMS, returns data."""
 
+
+class IServiceIdentificationMetadata:
+    """OO-interface to WMS metadata.
+
+    Properties
+    ----------
+    service : string
+        "WMS", "WFS".
+    version : string
+        Version of service protocol.
+    title : string
+        Human-targeted title of service.
+    abstract : string
+        Text describing the service.
+    keywords : list
+        List of strings.
+    rights : list
+        Explanation of rights associated with service.
+    """
+    
+class IServiceProviderMetadata:
+    """OO-interface to WMS metadata.
+
+    Properties
+    ----------
+    provider : string
+        Organization name.
+    url : string
+        URL for provider web site.
+    contact : string
+        How to contact the service provider.
+    """
+
+class IServiceOperationsMetadata:
+    """OO-interface to WMS metadata.
+
+    Properties
+    ----------
+    operations : list
+        List of operation descriptors.
+    """
+    
+class IOperationsMetadata:
+    """OO-interface to WMS metadata.
+
+    Properties
+    ----------
+    name : string
+        "GetCapabilities", for example.
+    methods : dict
+        Array of method descriptors, keyed to HTTP verbs.
+    """
+
+class IMethodMetadata:
+    """OO-interface to WMS metadata.
+
+    Properties
+    ----------
+    url : string
+        Method URL.
+    
+    TODO: constraint
+    """
+
+class IContentsMetadata:
+    """OO-interface to WMS metadata.
+
+    Properties
+    ----------
+    contents : list
+        List of content descriptors.
+    """
+
+class IContentMetadata:
+    """OO-interface to WMS metadata.
+
+    Properties
+    ----------
+    id : string
+        Unique identifier.
+    title : string
+        Human-targeted title.
+    boundingBox : 5-tuple
+        Four bounding values and a coordinate reference system identifier.
+    boundingBoxWGS84 : 4-tuple
+        Four bounding values in WGS coordinates.
+    formatOptions : list
+        List of available formats, each a string.
+    crsOptions : list
+        List of available coordinate/spatial reference systems.
+    """
 
