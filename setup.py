79a9f6a671e84b0f8176b7a164f821b5fe50fe9a
seang <seang@b426a367-1105-0410-b9ff-cdf4ab011145>
2006-10-18 23:08:26 +0000

    get down to one setup file, improve the readme
    git-svn-id: https://owslib.svn.sourceforge.net/svnroot/owslib/OWSLib/trunk@524 b426a367-1105-0410-b9ff-cdf4ab011145

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils import setup
 
 setup(name          = 'OWSLib',
-      version       = '0.0',
+      version       = '0.1.0',
       description   = 'OGC Web Service utility package',
+      license       = 'GPL',
+      keywords      = 'ogc ows wfs wms capabilities metadata',
       author        = 'Sean Gillies',
       author_email  = 'sgillies@frii.com',
+      maintainer        = 'Sean Gillies',
+      maintainer_email  = 'sgillies@frii.com',
       url           = 'http://www.gispython.org',
-      packages      = ['owslib']
+      packages      = ['owslib'],
+      classifiers   = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GPL License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: GIS',
+        ],
 )
 
