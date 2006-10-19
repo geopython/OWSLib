c61715582159e9e26afda6cc3e5b1974933538f8
Sean Gillies <sean.gillies@gmail.com>
2006-10-19 17:40:15 +0000

    fixed broken classifier, updated changes and version

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils import setup
+from setuptools import setup
 
 setup(name          = 'OWSLib',
       version       = '0.1.0',
-      description   = 'OGC Web Service utility package',
+      description   = 'OGC Web Service utility library',
       license       = 'GPL',
-      keywords      = 'ogc ows wfs wms capabilities metadata',
+      keywords      = 'gis ogc ows wfs wms capabilities metadata',
       author        = 'Sean Gillies',
       author_email  = 'sgillies@frii.com',
       maintainer        = 'Sean Gillies',
       maintainer_email  = 'sgillies@frii.com',
-      url           = 'http://www.gispython.org',
+      url           = 'http://trac.gispython.org/projects/PCL/wiki/OwsLib',
       packages      = ['owslib'],
       classifiers   = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GPL License',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Scientific/Engineering :: GIS',
