8ee52638ac610b413e8dc417917cfb984431ba8d
domlowe <domlowe@b426a367-1105-0410-b9ff-cdf4ab011145>
2008-04-03 08:36:54 +0000

    Tested revision 983 ok, minor fix for revision 982 # continued

--- wcstemp/owslib/wms.py
+++ wcstemp/owslib/wms.py
@@ -234,7 +234,7 @@ class ServiceProvider(object):
 	contact = self._root.find('ContactInformation')
 	## sometimes there is a contact block that is empty, so make
 	## sure there are children to parse
-	if contact is not None and contact.getchildren():
+	if contact is not None and contact.getchildren() is not None:
             self.contact = ContactMetadata(contact)
         else:
             self.contact = None
