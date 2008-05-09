c0cad9d86a6b91d084d3acc391e65268cab46dd4
seang <seang@b426a367-1105-0410-b9ff-cdf4ab011145>
2008-05-09 14:55:12 +0000

    Restore setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
-
 from setuptools import setup
+
+readme = open('README.txt', 'rb').read()
 
 setup(name          = 'OWSLib',
       version       = '0.3',
       description   = 'OGC Web Service utility library',
+      long_description = readme,
       license       = 'BSD',
       keywords      = 'gis ogc ows wfs wms capabilities metadata',
       author        = 'Sean Gillies',
       author_email  = 'sgillies@frii.com',
       maintainer        = 'Sean Gillies',
       maintainer_email  = 'sgillies@frii.com',
-      url           = 'http://trac.gispython.org/projects/PCL/wiki/OWSLib',
+      url           = 'http://trac.gispython.org/projects/PCL/wiki/OwsLib',
       packages      = ['owslib'],
       test_suite    = 'tests.test_suite',
       classifiers   = [
