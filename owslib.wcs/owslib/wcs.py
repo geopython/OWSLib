5703c9a2e712ad77202143658d9dae7f559cc27c
domlowe <domlowe@b426a367-1105-0410-b9ff-cdf4ab011145>
2009-10-28 13:16:27 +0000

    more moving stuff around in namespace branch

--- owslib.wcs/owslib/wcs.py
+++ owslib.wcs/owslib/wcs.py
@@ -1,5 +1,36 @@
+# -*- coding: ISO-8859-15 -*-
+# =============================================================================
+# Copyright (c) 2004, 2006 Sean C. Gillies
+# Copyright (c) 2007 STFC <http://www.stfc.ac.uk>
+#
+# Authors : 
+#          Dominic Lowe <d.lowe@rl.ac.uk>
+#
+# Contact email: d.lowe@rl.ac.uk
+# =============================================================================
 
-class WebCoverageService(object):
-  def __init__(self):
-    pass
-  
+"""
+Web Coverage Server (WCS) methods and metadata. Factory function.
+"""
+
+
+import urllib2
+import etree
+from coverage import wcs100, wcs110, wcsBase
+
+def WebCoverageService(url, version=None, xml=None):
+    ''' wcs factory function, returns a version specific WebCoverageService object '''
+    if xml is None:
+        reader = wcsBase.WCSCapabilitiesReader()
+        request = reader.capabilities_url(url)
+        xml = urllib2.urlopen(request).read()
+
+    if version is None:
+        capabilities = etree.etree.fromstring(xml)
+        version = capabilities.get('version')
+        del capabilities
+    
+    if version == '1.0.0':
+        return wcs100.WebCoverageService_1_0_0.__new__(wcs100.WebCoverageService_1_0_0, url, xml)
+    elif version == '1.1.0':
+        return wcs110.WebCoverageService_1_1_0.__new__(wcs110.WebCoverageService_1_1_0,url, xml)
