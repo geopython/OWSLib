79bea5afb472b0ee9dc508ea9ab9f262ad004c6c
lcinquini <lcinquini@b426a367-1105-0410-b9ff-cdf4ab011145>
2012-06-05 21:13:58 +0000

    Merging wps branch (r1868) back into trunk (r1866)

---
+++ examples/wps-ceda-script.py
@@ -0,0 +1,42 @@
+# Example script that performs a set of (small) live requests versus the live CEDA WPS service
+
+from owslib.wps import WebProcessingService, WPSExecution, WFSFeatureCollection, WFSQuery, GMLMultiPolygonFeatureCollection, monitorExecution, ComplexData, printInputOutput
+from owslib.util import dump
+
+verbose = True
+wps = WebProcessingService('http://ceda-wps2.badc.rl.ac.uk/wps', verbose=verbose)
+
+# 1) GetCapabilities
+# GET request: http://ceda-wps2.badc.rl.ac.uk/wps?Service=WPS&Request=GetCapabilities&Format=text/xml
+wps.getcapabilities()
+
+print 'WPS Identification type: %s' % wps.identification.type
+print 'WPS Identification title: %s' % wps.identification.title
+print 'WPS Identification abstract: %s' % wps.identification.abstract
+for operation in wps.operations:
+    print 'WPS Operation: %s' % operation.name
+for process in wps.processes:
+    print 'WPS Process: identifier=%s title=%s' % (process.identifier, process.title)
+    
+# 2) DescribeProcess
+# GET request: http://ceda-wps2.badc.rl.ac.uk/wps?identifier=DoubleIt&version=1.0.0&request=DescribeProcess&service=WPS
+process = wps.describeprocess('DoubleIt')
+print 'WPS Process: identifier=%s' % process.identifier
+print 'WPS Process: title=%s' % process.title
+print 'WPS Process: abstract=%s' % process.abstract
+for input in process.dataInputs:
+    print 'Process input:'
+    printInputOutput(input, indent='\t')
+for output in process.processOutputs:
+    print 'Process output:'
+    printInputOutput(output, indent='\t')
+
+# 3) Execute
+# POST request:
+# Note: not working, requires openid login ?
+#processid = "DoubleIt"
+#inputs = [ ("NumberToDouble","1") ]
+#output = "OutputXML"
+#execution = wps.execute(processid, inputs, output)
+
+#monitorExecution(execution)
---
+++ examples/wps-pml-script-1.py
@@ -0,0 +1,47 @@
+# Example script that performs a set of (small) live requests versus the live PML WPS service
+
+from owslib.wps import WebProcessingService, monitorExecution
+
+# instantiate WPS client
+verbose = False
+wps = WebProcessingService('http://rsg.pml.ac.uk/wps/generic.cgi', verbose=verbose)
+
+# 1) GetCapabilities
+wps.getcapabilities()
+print 'WPS Identification type: %s' % wps.identification.type
+print 'WPS Identification title: %s' % wps.identification.title
+print 'WPS Identification abstract: %s' % wps.identification.abstract
+for operation in wps.operations:
+    print 'WPS Operation: %s' % operation.name
+for process in wps.processes:
+    print 'WPS Process: identifier=%s title=%s' % (process.identifier, process.title)
+    
+# 2) DescribeProcess
+process = wps.describeprocess('reprojectImage')
+print 'WPS Process: identifier=%s' % process.identifier
+print 'WPS Process: title=%s' % process.title
+print 'WPS Process: abstract=%s' % process.abstract
+for input in process.dataInputs:
+    print 'Process input: identifier=%s, data type=%s, minOccurs=%d, maxOccurs=%d' % (input.identifier, input.dataType, input.minOccurs, input.maxOccurs)
+for output in process.processOutputs:
+    print 'Process output: identifier=%s, data type=%s' % (output.identifier, output.dataType)
+    
+# 3a) Execute
+# GET request: http://rsg.pml.ac.uk/wps/generic.cgi?request=Execute&service=wps&version=1.0.0&identifier=reprojectImage&datainputs=[inputImage=http://rsg.pml.ac.uk/wps/testdata/elev_srtm_30m.img;outputSRS=EPSG:4326]&responsedocument=outputImage=@asreference=true
+processid = "reprojectImage"
+inputs = [ ("inputImage","http://rsg.pml.ac.uk/wps/testdata/elev_srtm_30m.img"),
+           ("outputSRS", "EPSG:4326") ]
+output = "outputImage"
+execution = wps.execute(processid, inputs, output)
+
+monitorExecution(execution)
+        
+# 3b) Execute
+# GET request: http://rsg.pml.ac.uk/wps/generic.cgi?request=Execute&service=WPS&version=1.0.0&identifier=reprojectCoords&datainputs=[coords=http://rsg.pml.ac.uk/wps/testdata/coords.txt;outputSRS=EPSG:32630;inputSRS=EPSG:4326]
+processid = "reprojectCoords"
+inputs = [ ("coords","http://rsg.pml.ac.uk/wps/testdata/coords.txt"),
+           ("outputSRS", "EPSG:32630"),
+           ("inputSRS","EPSG:4326") ]
+execution = wps.execute(processid, inputs)
+
+monitorExecution(execution)
