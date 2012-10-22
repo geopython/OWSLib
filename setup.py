6768f4cb475fb3ca7be94385a8309d913c4de57c
Tom Kralidis <tomkralidis@hotmail.com>
2012-10-22 18:58:06 -0400

    add __version__

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
+import owslib
 
 readme = open('README.txt', 'rb').read()
 
 setup(name          = 'OWSLib',
-      version       = '0.5-dev',
+      version       = owslib.__version__,
       description   = 'OGC Web Service utility library',
       long_description = readme,
       license       = 'BSD',
