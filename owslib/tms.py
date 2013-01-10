435e0030215ae6ec5badb5588e4fd5e03a444a4a
Christian Ledermann <christian.ledermann@gmail.com>
2013-01-10 20:03:40 +0300

    get layer metadata

--- owslib/tms.py
+++ owslib/tms.py
@@ -12,14 +12,24 @@
 # Contact email: sgillies@frii.com
 # =============================================================================
 
-
-# http://osgeo-org.1560.n6.nabble.com/OSGEO-code-td3852851.html
-# "EPSG:900913" = ["OSGEO:41001", "EPSG:3785", "EPSG:3857", "EPSG:54004"]
-
+# TMS as defined in:
+# http://wiki.osgeo.org/wiki/Tile_Map_Service_Specification
 
 from etree import etree
 from .util import openURL, testXMLValue
 
+
+def force900913(epsg):
+# http://osgeo-org.1560.n6.nabble.com/OSGEO-code-td3852851.html
+# "EPSG:900913" = ["OSGEO:41001", "EPSG:3785", "EPSG:3857", "EPSG:54004"]
+    if epsg.upper() in ["OSGEO:41001", "EPSG:3785", "EPSG:3857", "EPSG:54004"]:
+        return "EPSG:900913"
+    else:
+        return epsg
+
+
+class ServiceException(Exception):
+    pass
 
 class TileMapService(object):
     """Abstraction for OGC Tile Map Service (TMS).
@@ -50,13 +60,31 @@ class TileMapService(object):
         # build metadata objects
         self._buildMetadata(parse_remote_metadata)
 
+
+    def _getcapproperty(self):
+        if not self._capabilities:
+            reader = TMSCapabilitiesReader(
+                self.version, url=self.url, un=self.username, pw=self.password
+                )
+            self._capabilities = ServiceMetadata(reader.read(self.url))
+        return self._capabilities
+
+
     def _buildMetadata(self, parse_remote_metadata=False):
         ''' set up capabilities metadata objects '''
         if self._capabilities.attrib.get('version'):
             self.version = self._capabilities.attrib.get('version')
         self.identification=ServiceIdentification(self._capabilities, self.version)
 
-        #import ipdb; ipdb.set_trace()
+        self.contents={}
+        tilemaps = self._capabilities.find('TileMaps')
+        if tilemaps is not None:
+            for tilemap in tilemaps.findall('TileMap'):
+                cm = ContentMetadata(tilemap, un=self.username, pw=self.password)
+                if cm.id:
+                    if cm.id in self.contents:
+                        raise KeyError('Content metadata for layer "%s" already exists' % cm.id)
+                    self.contents[cm.id] = cm
 
     def getServiceXML(self):
         xml = None
@@ -65,18 +93,96 @@ class TileMapService(object):
         return xml
 
 class ServiceIdentification(object):
-    ''' Implements IServiceIdentificationMetadata '''
 
     def __init__(self, infoset, version):
         self._root=infoset
+        if self._root.tag != 'TileMapService':
+            raise ServiceException
         self.version = version
         self.title = testXMLValue(self._root.find('Title'))
         self.abstract = testXMLValue(self._root.find('Abstract'))
         self.keywords = []
         f = self._root.find('KeywordList')
-        if f:
+        if f is not None:
             self.keywords = f.text.split()
         self.url = self._root.attrib.get('services')
+
+
+class ContentMetadata(object):
+    """
+    Abstraction for TMS layer metadata.
+    """
+    def __str__(self):
+        return 'Layer Title: %s, URL: %s' % (self.title, self.id)
+
+    def __init__(self, elem, un=None, pw=None):
+        if elem.tag != 'TileMap':
+            raise ValueError('%s should be a TileMap' % (elem,))
+        self.id = elem.attrib['href']
+        self.title = elem.attrib['title']
+        self.srs = force900913(elem.attrib['srs'])
+        self.profile = elem.attrib['profile']
+        self.password = pw
+        self.username = pw
+        self._tile_map = None
+        self._tm_elem = None
+
+    def _get_tilemap(self):
+        if self._tile_map is None:
+            u = openURL(self.id, '', method='Get', username = self.username, password = self.password)
+            elem = etree.fromstring(u.read())
+            self._tm_elem = elem
+            if elem.tag != 'TileMap':
+                raise ValueError('%s should be a TileMap' % (elem,))
+            self._tile_map ={}
+            self._tile_map['title'] = testXMLValue(elem.find('Title'))
+            self._tile_map['abstract'] = testXMLValue(elem.find('Abstract'))
+            self._tile_map['srs'] = force900913(testXMLValue(elem.find('SRS')))
+            assert(self._tile_map['srs'] == self.srs)
+            bbox = elem.find('BoundingBox')
+            self._tile_map['boundingBox'] = (bbox.attrib['minx'],
+                                            bbox.attrib['miny'],
+                                            bbox.attrib['maxx'],
+                                            bbox.attrib['maxy'])
+            origin = elem.find('Origin')
+            self._tile_map['origin'] = (origin.attrib['x'], origin.attrib['y'])
+            tf = elem.find('TileFormat')
+            self._tile_map['width'] = tf.attrib['width']
+            self._tile_map['height'] = tf.attrib['height']
+            self._tile_map['mime-type'] = tf.attrib['mime-type']
+            self._tile_map['extension'] = tf.attrib['extension']
+        return self._tile_map
+
+
+    @property
+    def abstract(self):
+        return self._get_tilemap()['abstract']
+
+    @property
+    def width(self):
+        return self._get_tilemap()['width']
+
+    @property
+    def height(self):
+        return self._get_tilemap()['height']
+
+    @property
+    def mimetype(self):
+        return self._get_tilemap()['mime-type']
+
+    @property
+    def extension(self):
+        return self._get_tilemap()['extension']
+
+    @property
+    def boundingBox(self):
+        return self._get_tilemap()['boundingBox']
+
+    @property
+    def origin(self):
+        return self._get_tilemap()['origin']
+
+
 
 
 class TMSCapabilitiesReader(object):
