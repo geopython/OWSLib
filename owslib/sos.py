c4663034e9cece92d1ec2826edcb740f76b0cf5a
Tom Kralidis <tomkralidis@hotmail.com>
2013-02-14 20:57:36 -0500

    remove parse_metadata arg

--- owslib/sos.py
+++ owslib/sos.py
@@ -13,10 +13,10 @@ Sensor Observation Service (SOS) methods and metadata. Factory function.
 
 from swe.observation import sos100
 
-def SensorObservationService(url, version='1.0.0', xml=None, parse_remote_metadata=False):
+def SensorObservationService(url, version='1.0.0', xml=None):
     """sos factory function, returns a version specific SensorObservationService object"""
     if version in  ['1.0', '1.0.0']:
-        return sos100.SensorObservationService_1_0_0.__new__(sos100.SensorObservationService_1_0_0, url, version, xml, parse_remote_metadata)
+        return sos100.SensorObservationService_1_0_0.__new__(sos100.SensorObservationService_1_0_0, url, version, xml)
 #    elif version in ['2.0', '2.0.0']:
-#        return sos200.SensorObservationService_2_0_0.__new__(sos200.SensorObservationService_2_0_0, url, version, xml, parse_remote_metadata)
+#        return sos200.SensorObservationService_2_0_0.__new__(sos200.SensorObservationService_2_0_0, url, version, xml)
 
