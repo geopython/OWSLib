e1368fda55463e083ddd0f0ebf9bf8eb3e774b7d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2013-09-10 10:50:29 +0200

    update wps climdaps example

--- examples/wps-climdaps.py
+++ examples/wps-climdaps.py
@@ -10,6 +10,8 @@ inputs = [("input1", '1'), ("input2", '2')]
 # list of tuple (output identifier, asReference attribute)
 outputs = [("output1",True), ("output2",False)]
 
+wps.
+
 execution = wps.execute(processid, inputs, output=outputs)
 print execution.status
 # show status
@@ -19,15 +21,15 @@ print 'status message', execution.statusMessage
 monitorExecution(execution)
 
 for output in execution.processOutputs:
-	print 'identifier=%s, dataType=%s, data=%s, reference=%s' % (output.identifier, output.dataType, output.data, output.reference) 
+        print 'identifier=%s, dataType=%s, data=%s, reference=%s' % (output.identifier, output.dataType, output.data, output.reference) 
 
 # get errors
-inputs = [("input1", '1'), ("input2", 'aaa')]
+inputs = [("input1", '1'), ("input2", '3')]
 execution = wps.execute(processid, inputs, output=outputs)
 monitorExecution(execution)
 print execution.status
 for error in execution.errors:
-	print error.code, error.locator, error.text
+        print error.code, error.locator, error.text
 
 
 
