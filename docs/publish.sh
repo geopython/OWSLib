9b6098043c569fd58b58b0850f246bf042c89a18
Tom Kralidis <tomkralidis@gmail.com>
2014-10-13 18:58:34 -0400

    safeguard doc builds

--- docs/publish.sh
+++ docs/publish.sh
@@ -4,8 +4,8 @@
 THIS_DIR=`pwd`
 
 make clean && make html
-git clone git@github.com:geopython/OWSLib.git /tmp/OWSLib
-cd /tmp/OWSLib
+git clone git@github.com:geopython/OWSLib.git /tmp/OWSLib-$$
+cd /tmp/OWSLib-$$
 git checkout gh-pages
 /bin/cp -rp $THIS_DIR/build/html/en/* .
 git add .
@@ -13,4 +13,4 @@ git commit -am "update live docs [ci skip]"
 git push origin gh-pages
 
 cd $THIS_DIR
-rm -fr /tmp/OWSLib
+rm -fr /tmp/OWSLib-$$
