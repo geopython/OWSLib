3a56ec52a436ea3ba893d2b5c878f32445b2003f
pesekon2 <ondra.lobo@seznam.cz>
2017-05-31 15:37:30 +0200

    SOS handle username and password

--- owslib/sos.py
+++ owslib/sos.py
@@ -16,10 +16,25 @@ from __future__ import (absolute_import, division, print_function)
 from .swe.observation import sos100, sos200
 
 
-def SensorObservationService(url, version='1.0.0', xml=None):
-    """sos factory function, returns a version specific SensorObservationService object"""
-    if version in  ['1.0', '1.0.0']:
-        return sos100.SensorObservationService_1_0_0.__new__(sos100.SensorObservationService_1_0_0, url, version, xml)
+def SensorObservationService(url,
+                             version='1.0.0',
+                             xml=None,
+                             username=None,
+                             password=None,):
+    """
+    SOS factory function
+    :param url: url of capabilities document
+    :param version: SOS version 1.0.0 or 2.0.0
+    :param xml: elementtree object
+    :param username: username allowed to handle with SOS
+    :param password: password for the username
+    :return: a version specific SensorObservationService object
+    """
+    if version in ['1.0', '1.0.0']:
+        return sos100.SensorObservationService_1_0_0.__new__(
+            sos100.SensorObservationService_1_0_0, url, version,
+            xml, username, password)
     elif version in ['2.0', '2.0.0']:
-        return sos200.SensorObservationService_2_0_0.__new__(sos200.SensorObservationService_2_0_0, url, version, xml)
-
+        return sos200.SensorObservationService_2_0_0.__new__(
+            sos200.SensorObservationService_2_0_0, url, version,
+            xml, username, password)
