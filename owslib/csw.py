507e92af8304ebcf66054c4fd268746c03dcc39a
Tom Kralidis <tomkralidis@gmail.com>
2017-06-30 21:40:13 +0000

    fix ref

--- owslib/csw.py
+++ owslib/csw.py
@@ -649,7 +649,7 @@ class CatalogueServiceWeb(object):
                             # Well, just use the first one.
                             request_url = post_verbs[0].get('url')
                     elif len(post_verbs) == 1:
-                        request_post_url = post_verbs[0].get('url')
+                        request_url = post_verbs[0].get('url')
             except:  # no such luck, just go with request_url
                 pass
 
