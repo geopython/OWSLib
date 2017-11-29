7dfbf8c00987243933289971f46ad7778e903a11
Roel Huybrechts <roel.huybrechts@realdolmen.com>
2017-11-29 20:39:50 +0100

    Avoid double None-check for mdelem in WFS 2.0.0 too.

--- owslib/feature/wfs200.py
+++ owslib/feature/wfs200.py
@@ -436,13 +436,13 @@ class ContentMetadata:
 
                     mdelem = doc.find('.//metadata')
                     if mdelem:
-                        metadataUrl['metadata'] = Metadata(mdelem) if mdelem else None
+                        metadataUrl['metadata'] = Metadata(mdelem)
                         continue
 
                     mdelem = doc.find('.//' + util.nspath_eval('gmd:MD_Metadata', n.get_namespaces(['gmd']))) \
                              or doc.find('.//' + util.nspath_eval('gmi:MI_Metadata', n.get_namespaces(['gmi'])))
                     if mdelem:
-                        metadataUrl['metadata'] = MD_Metadata(mdelem) if mdelem else None
+                        metadataUrl['metadata'] = MD_Metadata(mdelem)
                         continue
                 except:
                     metadataUrl['metadata'] = None
