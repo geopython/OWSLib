96d132ec0cbdb8bc4b2e305f1d1cbb836127b069
Tom Kralidis <tomkralidis@gmail.com>
2018-03-20 14:18:38 -0400

    first pass fix CSW tests

---
+++ tests/test_csw_geoserver.py
@@ -0,0 +1,18 @@
+from __future__ import (absolute_import, division, print_function)
+
+import pytest
+import requests
+
+from owslib.csw import CatalogueServiceWeb
+
+SERVICE_URL = 'http://gis.armf.bg:8080/geoserver/csw'
+
+@pytest.mark.online
+@pytest.mark.skipif(not requests.get(SERVICE_URL).ok,
+                    reason='service is unreachable')
+def test_csw_geoserver():
+    c = CatalogueServiceWeb(SERVICE_URL)
+    c.getrecords2(typenames='csw:Record')
+    assert c.results.get('returned') > 0
+    assert c.results.get('nextrecord') > 0
+    assert c.results.get('matches') > 0
