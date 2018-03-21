f88cd071bc7649eb21d401366718e34c9b375816
Tom Kralidis <tomkralidis@gmail.com>
2018-03-21 05:41:49 -0400

    first pass fix CSW tests (#422)
    * first pass fix CSW tests
    * restore test_csw_ngdc.py remove __future__ statements

--- tests/test_csw_ngdc.py
+++ tests/test_csw_ngdc.py
@@ -1,4 +1,3 @@
-from __future__ import (absolute_import, division, print_function)
 from owslib import fes, csw
 from owslib.dif import namespaces
 
---
+++ tests/test_csw_pycsw_skip_caps.py
@@ -0,0 +1,16 @@
+import pytest
+import requests
+
+from owslib.csw import CatalogueServiceWeb as cs
+
+SERVICE_URL = 'http://demo.pycsw.org/cite/csw'
+
+@pytest.mark.online
+@pytest.mark.skipif(not requests.get(SERVICE_URL).ok,
+                    reason='service is unreachable')
+def test_csw_pycsw_skip_caps():
+    c = cs(SERVICE_URL, skip_caps=True)
+    c.getrecords2()
+    assert c.results.get('returned') > 0
+    assert c.results.get('nextrecord') > 0
+    assert c.results.get('matches') > 0
