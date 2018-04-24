d07d3da4d61d4960b82b06d5406a6c7d5f573c8c
Pingu Carsti <ehbrecht@dkrz.de>
2018-04-24 18:13:34 +0200

    Fix #333: add overwrite option to build_get_url

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,9 +1,9 @@
 import pytest
 
-from owslib.util import clean_ows_url
+from owslib.util import clean_ows_url, build_get_url
 
 
-def test_util():
+def test_clean_ows_url():
     assert clean_ows_url('http//example.org/wms') == 'http//example.org/wms'
     assert clean_ows_url('http//example.org/wms?service=WMS') == 'http//example.org/wms'
     assert clean_ows_url('http//example.org/wms?SERVICE=WMS') == 'http//example.org/wms'
@@ -16,3 +16,24 @@ def test_util():
 def test_util_py34():
     assert clean_ows_url('http://example.org/wms?map=/path/to/foo.map&SERVICE=WMS&version=1.3.0&request=GetCapabilities') == 'http://example.org/wms?map=%2Fpath%2Fto%2Ffoo.map'  # noqa
     assert clean_ows_url('http://example.org/wms?map=/path/to/foo.map&foo=bar&&SERVICE=WMS&version=1.3.0&request=GetCapabilities') == 'http://example.org/wms?map=%2Fpath%2Fto%2Ffoo.map&foo=bar'  # noqa
+
+
+def test_build_get_url():
+    assert build_get_url("http://example.org/wps", {'service': 'WPS'}) == 'http://example.org/wps?service=WPS'
+    assert build_get_url("http://example.org/wms", {'SERVICE': 'wms'}) == 'http://example.org/wms?SERVICE=wms'
+    assert build_get_url("http://example.org/wps?service=WPS", {'request': 'GetCapabilities'}) == \
+        'http://example.org/wps?service=WPS&request=GetCapabilities'
+    assert build_get_url("http://example.org/wps?service=WPS", {'request': 'GetCapabilities'}) == \
+        'http://example.org/wps?service=WPS&request=GetCapabilities'
+    # Can not overwrite parameter
+    assert build_get_url("http://example.org/ows?SERVICE=WPS", {'SERVICE': 'WMS'}) == \
+        'http://example.org/ows?SERVICE=WPS'
+    # Parameter is case-senstive
+    assert build_get_url("http://example.org/ows?SERVICE=WPS", {'service': 'WMS'}) == \
+        'http://example.org/ows?SERVICE=WPS&service=WMS'
+
+
+def test_build_get_url_overwrite():
+    # Use overwrite flag
+    assert build_get_url("http://example.org/ows?SERVICE=WPS", {'SERVICE': 'WMS'}, overwrite=True) == \
+        'http://example.org/ows?SERVICE=WMS'
