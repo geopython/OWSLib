f4fab888b2c0002d0bc501e12c7daffa46d6e336
Pingu Carsti <ehbrecht@dkrz.de>
2018-05-14 15:11:59 +0200

    Fix #248: sos encoding error

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,7 +1,14 @@
+# -*- coding: UTF-8 -*-
 import pytest
 
 import codecs
-from owslib.util import clean_ows_url, build_get_url, strip_bom
+from owslib.util import clean_ows_url, build_get_url, strip_bom, encode_string
+
+
+def test_encode_string():
+    assert encode_string('hello') == 'hello'
+    assert encode_string('Particulate Matter < 10 µm') == 'Particulate Matter < 10 µm'
+    assert encode_string(u'Particulate Matter < 10 µm') == 'Particulate Matter < 10 µm'
 
 
 def test_strip_bom():
