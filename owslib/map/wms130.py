5010c99c38e748a9cc0ec10be8c5789b89cb554a
Denis Rykov <rykovd@gmail.com>
2018-12-06 17:46:22 +0100

    OnlineResource element within the Service element is optional

--- owslib/map/wms130.py
+++ owslib/map/wms130.py
@@ -411,7 +411,10 @@ class ServiceProvider(object):
             self.name = name.text
         else:
             self.name = None
-        self.url = self._root.find(nspath('OnlineResource', WMS_NAMESPACE)).attrib.get('{http://www.w3.org/1999/xlink}href', '')
+        self.url = None
+        online_resource = self._root.find(nspath('OnlineResource', WMS_NAMESPACE))
+        if online_resource is not None:
+            self.url = online_resource.attrib.get('{http://www.w3.org/1999/xlink}href', '')
         # contact metadata
         contact = self._root.find(nspath('ContactInformation', WMS_NAMESPACE))
         # sometimes there is a contact block that is empty, so make
