b9507231dd26c8d0cc4c9c362d633677a84043e5
David Huard <david.huard@gmail.com>
2019-01-09 10:00:43 -0500

    fix test for emu dp all

--- tests/test_wps_describeprocess_emu_all.py
+++ tests/test_wps_describeprocess_emu_all.py
@@ -4,10 +4,10 @@ from owslib.wps import WebProcessingService, Process
 
 def test_wps_describeprocess_emu_all():
     # Initialize WPS client
-    wps = WebProcessingService('http://localhost:5000', skip_caps=True)
+    wps = WebProcessingService('http://localhost:8094/wps', skip_caps=True)
     # Execute fake invocation of DescribeProcess operation by parsing cached response from
     xml = open(resource_file('wps_EmuDescribeProcess_all.xml'), 'rb').read()
-    process = wps.describeprocess('nap')
-    processes = wps.describeprocess('all')
+    process = wps.describeprocess('nap', xml=xml)
+    processes = wps.describeprocess('all', xml=xml)
     assert isinstance(process, Process)
     assert isinstance(processes, list)
