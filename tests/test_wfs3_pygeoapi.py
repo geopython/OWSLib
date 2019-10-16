0bc8512f0407dd209c4052dca25405329d7c2676
Tom Kralidis <tomkralidis@gmail.com>
2019-10-16 07:04:51 -0400

    WFS3: fix tests to pass limit as positive integer

--- tests/test_wfs3_pygeoapi.py
+++ tests/test_wfs3_pygeoapi.py
@@ -35,7 +35,6 @@ def test_wfs3_pygeoapi():
     assert lakes['description'] == 'lakes of the world, public domain'
 
     lakes_query = w.collection_items('lakes', limit=1)
-    print(lakes_query)
     assert lakes_query['numberMatched'] == 25
     assert lakes_query['numberReturned'] == 1
     assert len(lakes_query['features']) == 1
