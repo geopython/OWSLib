2073201dcae2a9870382fd03f50fe81fb6746eea
Pingu Carsti <ehbrecht@dkrz.de>
2020-01-27 14:02:03 +0100

    updated test

--- tests/test_wms_datageo_130.py
+++ tests/test_wms_datageo_130.py
@@ -8,3 +8,10 @@ def test_wms_capabilities():
     # http://wms.jpl.nasa.gov/wms.cgi.
     xml = open(resource_file('wms_datageo_caps_130.xml'), 'rb').read()
     wms = WebMapService('url', version='1.3.0', xml=xml)
+
+    # Test capabilities
+    # -----------------
+
+    assert wms.identification.type == 'WMS'
+    assert wms.identification.version == '1.3.0'
+    assert wms.identification.title == 'GeoServer Web Map Service'
