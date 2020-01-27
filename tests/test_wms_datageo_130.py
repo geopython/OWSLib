52091ed397b6ed5ff805cfa6e54fd1c9c84639b2
Pingu Carsti <ehbrecht@dkrz.de>
2020-01-27 14:13:56 +0100

    update test

--- tests/test_wms_datageo_130.py
+++ tests/test_wms_datageo_130.py
@@ -5,7 +5,7 @@ from owslib.wms import WebMapService
 
 def test_wms_capabilities():
     # Fake a request to a WMS Server using saved doc from
-    # http://wms.jpl.nasa.gov/wms.cgi.
+    # http://datageo.ambiente.sp.gov.br/serviceTranslator/rest/getXml/IPT_Geoserver_WMS/Risco_Louveira/1449249453587/wms
     xml = open(resource_file('wms_datageo_caps_130.xml'), 'rb').read()
     wms = WebMapService('url', version='1.3.0', xml=xml)
 
