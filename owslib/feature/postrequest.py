6a7735ec78282ea45c739a8ffe73fa28e037dbee
f-PLT <pelletier.f@gmail.com>
2020-09-15 11:12:50 -0400

    Add docstring to postrequest.py

--- owslib/feature/postrequest.py
+++ owslib/feature/postrequest.py
@@ -22,14 +22,20 @@ class PostRequest():
         self._query = etree.SubElement(self._root, util.nspath('Query', namespace))
 
     def set_startindex(self, startindex):
+        """Set the starting index value for the request"""
         self._root.set("startIndex", str(startindex))
 
     def set_propertyname(self, propertyname):
+        """Set which feature properties will be returned.
+
+        If not set, will return all properties."""
         for pn in propertyname:
             etree.SubElement(self._query, "PropertyName").text = pn
 
     def to_string(self):
-        """Returns the xml request in string format"""
+        """Returns the xml request in string format.
+
+        Required in order to use the request with getfeature()"""
         return etree.tostring(self._root)
 
 
@@ -45,6 +51,10 @@ class PostRequest_1_1_0(PostRequest):
         self._query.set("typeName", typename)
 
     def set_bbox(self, bbox):
+        """Set a bbox filter.
+
+        Cannot be used with set_featureid() or set_filter().
+        """
         filter_tree = etree.SubElement(self._query, util.nspath('Filter', OGC_NAMESPACE))
         bbox_tree = etree.SubElement(filter_tree, util.nspath('BBOX', OGC_NAMESPACE))
         coords = etree.SubElement(bbox_tree, util.nspath('Envelope', GML_NAMESPACE))
@@ -56,6 +66,10 @@ class PostRequest_1_1_0(PostRequest):
                          ).text = '{} {}'.format(bbox[2], bbox[3])
 
     def set_featureid(self, featureid):
+        """Set filter by feature id.
+
+        Cannot be used with set_bbox() or set_filter().
+        """
         feature_tree = etree.SubElement(self._query, util.nspath('Filter', OGC_NAMESPACE))
 
         for ft in featureid:
@@ -64,17 +78,29 @@ class PostRequest_1_1_0(PostRequest):
             feature_tree.append(prop_id)
 
     def set_filter(self, filter):
+        """Set filter from existing filter.
+
+        Will integrate the filter tag of a provided xml filter to the current one
+        being built.
+
+        Cannot be used with set_bbox() or set_featureid().
+        """
         f = etree.fromstring(filter)
         sub_elem = f.find(util.nspath("Filter", OGC_NAMESPACE))
         self._query.append(sub_elem)
 
     def set_maxfeatures(self, maxfeatures):
+        """Set the maximum number of features to be returned."""
         self._root.set("maxFeatures", str(maxfeatures))
 
     def set_outputformat(self, outputFormat):
+        """Set the output format.
+
+        Verify the available formats with a GetCapabilites request."""
         self._root.set("outputFormat", outputFormat)
 
     def set_sortby(self, sortby):
+        """Set the properties by which the response will be sorted."""
         sort_tree = etree.SubElement(self._query, util.nspath("SortBy", OGC_NAMESPACE))
         for s in sortby:
             prop = etree.SubElement(sort_tree, util.nspath("SortProperty", OGC_NAMESPACE))
@@ -82,7 +108,7 @@ class PostRequest_1_1_0(PostRequest):
 
 
 class PostRequest_2_0_0(PostRequest):
-    """XML Post request payload builder for WFS version 2.0.0"""
+    """XML Post request payload builder for WFS version 2.0.0."""
 
     def __init__(self):
         super().__init__(version='2.0.0', namespace=WFS20_NAMESPACE)
@@ -93,6 +119,10 @@ class PostRequest_2_0_0(PostRequest):
         self._query.set("typenames", typename)
 
     def set_bbox(self, bbox):
+        """Set a bbox filter.
+
+        Cannot be used with set_featureid() or set_filter().
+        """
         filter_tree = etree.SubElement(self._query, util.nspath('Filter', FES_NAMESPACE))
         bbox_tree = etree.SubElement(filter_tree, util.nspath('BBOX', FES_NAMESPACE))
         coords = etree.SubElement(bbox_tree, util.nspath('Envelope', GML32_NAMESPACE))
@@ -104,6 +134,10 @@ class PostRequest_2_0_0(PostRequest):
                          ).text = '{} {}'.format(bbox[2], bbox[3])
 
     def set_featureid(self, featureid):
+        """Set filter by feature id.
+
+        Cannot be used with set_bbox() or set_filter().
+        """
         feature_tree = etree.SubElement(self._query, util.nspath('Filter', FES_NAMESPACE))
         for ft in featureid:
             prop_id = etree.Element(util.nspath('ResourceId', FES_NAMESPACE))
@@ -111,17 +145,30 @@ class PostRequest_2_0_0(PostRequest):
             feature_tree.append(prop_id)
 
     def set_filter(self, filter):
+        """Set filter from existing filter.
+
+        Will integrate the filter tag of a provided xml filter to the current one
+        being built.
+
+        Cannot be used with set_bbox() or set_featureid().
+        """
         f = etree.fromstring(filter)
         sub_elem = f.find(util.nspath("Filter", FES_NAMESPACE))
         self._query.append(sub_elem)
 
     def set_maxfeatures(self, maxfeatures):
+        """Set the maximum number of features to be returned."""
         self._root.set("count", str(maxfeatures))
 
     def set_outputformat(self, outputFormat):
+        """Set the output format.
+
+        Verify the available formats with a GetCapabilites request.
+        """
         self._root.set("outputformat", outputFormat)
 
     def set_sortby(self, sortby):
+        """Set the properties by which the response will be sorted."""
         sort_tree = etree.SubElement(self._query, util.nspath("SortBy", FES_NAMESPACE))
         for s in sortby:
             prop = etree.SubElement(sort_tree, util.nspath("SortProperty", FES_NAMESPACE))
