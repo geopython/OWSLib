f162c42a8ef876589701a42e6f9f2ec66df71563
Tom Kralidis <tomkralidis@gmail.com>
2020-12-08 21:18:40 -0500

    update live docs [ci skip]

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,11 +1,10 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.20.0',
+    VERSION: '0.21.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
-    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
