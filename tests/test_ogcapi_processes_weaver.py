fbec0bd6f54e07ce290e4577478e8942acba943c
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2021-02-18 10:37:44 -0500

    add minimal test case for weaver OGC-API example - missing conformace commented (crim-ca/weaver#200)

--- tests/test_ogcapi_processes_weaver.py
+++ tests/test_ogcapi_processes_weaver.py
@@ -16,25 +16,27 @@ def test_ogcapi_processes_weaver():
     assert w.url == 'https://ogc-ades.crim.ca/ADES/'
     assert w.url_query_string is None
 
-    # TODO: RuntimeError: Did not find service-desc link
-    api = w.api()
+    # TODO: RuntimeError: Did not find service-desc link (https://github.com/crim-ca/weaver/issues/200)
+    # api = w.api()
     # assert api['components']['parameters'] is not None
     # paths = api['paths']
     # assert paths is not None
     # assert paths['/processes/hello-world'] is not None
 
     conformance = w.conformance()
-    assert len(conformance['conformsTo']) == 5
+    assert len(conformance['conformsTo']) > 0 and all(isinstance(c, str) for c in conformance['conformsTo'])
 
     # list processes
     processes = w.processes()
     assert len(processes) > 0
 
     # process description
-    # TODO: response not as expected {'process': {'id': 'ColibriFlyingpigeon_SubsetBbox'}}
-    # should be {'id': 'ColibriFlyingpigeon_SubsetBbox'}
-    # process = w.process_description('ColibriFlyingpigeon_SubsetBbox')
-    # print(process)
-    # assert process['process']['id'] == 'ColibriFlyingpigeon_SubsetBbox'
-    # assert process['process']['title'] == 'ColibriFlyingpigeon_SubsetBbox'
-    # assert "An example process that takes a name as input" in process['process']['description']
+    # following are builtin processes and should always be available
+    for pid in ['file2string_array', 'jsonarray2netcdf', 'metalink2netcdf']:
+        process = w.process_description(pid)
+        proc_desc = process['process']
+        assert proc_desc['id'] == pid
+        assert 'inputs' in proc_desc and isinstance(proc_desc['inputs'], list) and len(proc_desc['inputs'])
+        assert all('id' in p_in and isinstance(p_in['id'], str) for p_in in proc_desc['inputs'])
+        assert 'outputs' in proc_desc and isinstance(proc_desc['outputs'], list) and len(proc_desc['outputs'])
+        assert all('id' in p_out and isinstance(p_out['id'], str) for p_out in proc_desc['outputs'])
