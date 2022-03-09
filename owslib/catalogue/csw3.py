214674e7185ff25ab9576fa5dba689a2844944d6
Tom Kralidis <tomkralidis@gmail.com>
2022-03-08 19:50:47 -0500

    CSW: add headers to CatalogueServiceWeb init

--- owslib/catalogue/csw3.py
+++ owslib/catalogue/csw3.py
@@ -43,7 +43,7 @@ schema_location = '%s %s' % (namespaces['csw30'], schema)
 class CatalogueServiceWeb(object):
     """ csw request class """
     def __init__(self, url, lang='en-US', version='3.0.0', timeout=10, skip_caps=False,
-                 username=None, password=None, auth=None):
+                 username=None, password=None, auth=None, headers=None):
         """
 
         Construct and process a GetCapabilities request
@@ -59,6 +59,7 @@ class CatalogueServiceWeb(object):
         - username: username for HTTP basic authentication
         - password: password for HTTP basic authentication
         - auth: instance of owslib.util.Authentication
+        - headers: HTTP headers to send with requests
 
         """
         if auth:
@@ -71,6 +72,7 @@ class CatalogueServiceWeb(object):
         self.version = version
         self.timeout = timeout
         self.auth = auth or Authentication(username, password)
+        self.headers = headers
         self.service = 'CSW'
         self.exceptionreport = None
         self.owscommon = ows.OwsCommon('2.0.0')
@@ -567,8 +569,11 @@ class CatalogueServiceWeb(object):
 
         if isinstance(self.request, str):  # GET KVP
             self.request = '%s%s' % (bind_url(request_url), self.request)
+            headers_ = {'Accept': outputformat}
+            if self.headers:
+                headers_.update(self.headers)
             self.response = openURL(
-                self.request, None, 'Get', timeout=self.timeout, auth=self.auth, headers={'Accept': outputformat}
+                self.request, None, 'Get', timeout=self.timeout, auth=self.auth, headers=headers_
             ).read()
         else:
             self.request = cleanup_namespaces(self.request)
