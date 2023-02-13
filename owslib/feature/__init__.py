167450fbbbd1ef41a565b32d8ccf9be68fa1f33e
geographika <sethg@geographika.co.uk>
2023-02-13 22:17:55 +0100

    use build_get_url

--- owslib/feature/__init__.py
+++ owslib/feature/__init__.py
@@ -9,6 +9,7 @@ import logging
 
 from urllib.parse import urlencode
 from owslib.crs import Crs
+from owslib.util import log, Authentication, build_get_url
 from owslib.util import Authentication
 from owslib.feature.schema import get_schema
 from owslib.feature.postrequest import PostRequest_1_1_0, PostRequest_2_0_0
@@ -209,7 +210,6 @@ class WebFeatureService_(object):
                 if m.get("type").lower() == method.lower()
             )
         )
-        base_url = base_url if base_url.endswith("?") else base_url + "?"
 
         request = {"service": "WFS", "version": self.version, "request": "GetFeature"}
 
@@ -248,9 +248,7 @@ class WebFeatureService_(object):
         if outputFormat is not None:
             request["outputFormat"] = outputFormat
 
-        data = urlencode(request, doseq=True)
-
-        return base_url + data
+        return build_get_url(base_url, request)
 
     def getPOSTGetFeatureRequest(
         self,
