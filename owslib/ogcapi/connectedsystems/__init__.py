6633f7c2a05ac74cdead8fd196d6bfd5ada81b34
Ian Patterson <cr31.dev@gmail.com>
2024-03-30 01:05:07 -0500

    [CSAPI] add systems

---
+++ owslib/ogcapi/connectedsystems/__init__.py
@@ -0,0 +1,85 @@
+# ==============================================================================
+#  Copyright (c) 2024 Ian Patterson
+#
+#  Author: Ian Patterson <ian@botts-inc.com>
+#
+#  Contact email: ian@botts-inc.com
+# ==============================================================================
+
+import json
+import logging
+import requests
+import yaml
+
+from owslib import __version__
+from owslib.util import (Authentication, http_delete, http_get, http_post, http_put)
+
+LOGGER = logging.getLogger(__name__)
+
+REQUEST_HEADERS = {
+    'User-Agent': f'OWSLib {__version__} (https://owslib.readthedocs.io)'
+}
+
+
+class QueryParams:
+
+    @staticmethod
+    def check_for_params(**kwargs) -> dict:
+        """
+        checks for query parameters in kwargs passed from some API parent method
+        @type id: string
+        @param id: identifier of the resource
+        @type bbox: list
+        @param bbox: list of minx, miny, maxx, maxy
+        @type datetime_: string
+        @param datetime_: time extent or time instant
+        @type geom: string
+        @param geom: WKT representation of geometry
+        @type q: list
+        @param q: free text query strings, comma separated
+        @type parent: list
+        @param parent: local IDs or unique IDs, only returns resources the have a parent resource with an identifier in the list
+        @type foi: list
+        @param foi: local IDs or unique IDs, only returns resources that are associated with a feature of interest with an identifier in the list
+        @type observedProperty: list
+        @param observedProperty: local IDs or unique IDs, only returns resources that are associated with an observed property with an identifier in the list
+        @type controlledProperty: list
+        @param controlledProperty: local IDs or unique IDs, only returns resources that are associated with a controlled property with an identifier in the list
+        @type recursive: boolean
+        @param recursive: default false, if true, instructs server to include subsystems in the response
+        @type limit: integer
+        @param limit: default 10, maximum number of resources presented in the response
+        @type system: list
+        @param system: local IDs or unique IDs, only returns resources that are associated with a system with an identifier in the list
+
+        @returns: 'string' of comma separated query parameters
+        """
+
+        if 'id' in kwargs:
+            pass
+        if 'bbox' in kwargs:
+            kwargs['bbox'] = ','.join(list(map(str, kwargs['bbox'])))
+        if 'datetime_' in kwargs:
+            kwargs['datetime'] = kwargs['datetime']
+        if 'geom' in kwargs:
+            # TODO: make sure this serializes correctly
+            pass
+        if 'q' in kwargs:
+            kwargs['q'] = ','.join(list(map(str, kwargs['q'])))
+        if 'parent' in kwargs:
+            kwargs['parent'] = ','.join(list(map(str, kwargs['parent'])))
+        if 'foi' in kwargs:
+            kwargs['foi'] = ','.join(list(map(str, kwargs['foi'])))
+        if 'observedProperty' in kwargs:
+            kwargs['observedProperty'] = ','.join(list(map(str, kwargs['observedProperty'])))
+        if 'controlledProperty' in kwargs:
+            kwargs['controlledProperty'] = ','.join(list(map(str, kwargs['controlledProperty'])))
+        if 'recursive' in kwargs:
+            pass
+        if 'limit' in kwargs:
+            pass
+        if 'system' in kwargs:
+            kwargs['system'] = ','.join(list(map(str, kwargs['system'])))
+        return kwargs
+
+    
---
+++ owslib/ogcapi/connectedsystems/systems.py
@@ -0,0 +1,221 @@
+# ==============================================================================
+#  Copyright (c) 2024 Ian Patterson
+#
+#  Author: Ian Patterson <ian@botts-inc.com>
+#
+#  Contact email: ian@botts-inc.com
+# ==============================================================================
+
+import logging
+
+from owslib.ogcapi import Collections
+from owslib.util import Authentication
+
+LOGGER = logging.getLogger(__name__)
+
+
+# TODO: add query kwargs to all methods
+# TODO: perform checks against schema or add extra argument to account for the valid types
+# TODO: create tests
+# TODO: improve docstrings
+# TODO: may need to deactivate som collections features if CSAPI doesn't align totally with Common
+class Systems(Collections):
+    """Abstraction for OGC API - Connected Systems - Systems"""
+
+    def __init__(self, url: str, json_: str = None, timeout: int = 30,
+                 headers: dict = None, auth: Authentication = None):
+        __doc__ = Collections.__doc__  # noqa
+        super().__init__(url, json_, timeout, headers, auth)
+
+    def system_collections(self) -> list:
+        """
+        implements /collections filtered on systems
+
+        @returns: `list` of filtered collections object
+        """
+
+        systems_ = []
+        collections_ = super().collections()
+
+        for c_ in collections_['collections']:
+            if 'itemType' in c_ and c_['itemType'].lower() == 'system':
+                systems_.append(c_['id'])
+
+        return systems_
+
+    def collection_queryables(self, collection_id: str) -> dict:
+        """
+        implements /collections/{collectionId}/queryables
+
+        @type collection_id: string
+        @param collection_id: id of collection
+
+        @returns: `dict` of system collection queryables
+        """
+
+        path = f'collections/{collection_id}/queryables'
+        return self._request(path=path)
+
+    def collection_items(self, collection_id: str, **kwargs: dict) -> dict:
+        """
+        implements /collection/{collectionId}/items
+
+        @type collection_id: string
+        @param collection_id: id of collection
+
+        @returns: system collection results
+        """
+
+        path = f'collections/{collection_id}/items'
+        return self._request(path=path, **kwargs)
+
+    def collection_item(self, collection_id: str, item_id: str) -> dict:
+        """
+        implements /collection/{collectionId}/items/{itemId}
+
+        @type collection_id: string
+        @param collection_id: id of collection
+        @type item_id: string
+        @param item_id: id of item
+
+        @returns: system collection item result
+        """
+
+        path = f'collections/{collection_id}/items/{item_id}'
+        return self._request(path=path)
+
+    def collection_item_create(self, collection_id: str, data: str) -> dict:
+        """
+        implements POST /collection/{collectionId}/items
+
+        @type collection_id: string
+        @param collection_id: id of collection
+        @type data: string
+        @param data: raw representation of data
+
+        @returns: single item result
+        """
+
+        path = f'collections/{collection_id}/items'
+        return self._request(method='POST', path=path, data=data)
+
+
+    def systems(self) -> dict:
+        """
+        implements /systems
+
+        @returns: `dict` of systems object
+        """
+
+        path = 'systems'
+        return self._request(path=path)
+
+    def system(self, system_id: str) -> dict:
+        """
+        implements /systems/{systemId}
+
+        @type system_id: string
+        @param system_id: id of system
+
+        @returns: `dict` of system metadata
+        """
+
+        path = f'systems/{system_id}'
+        return self._request(path=path)
+
+    def system_create(self, system_id: str, data: str) -> dict:
+        """
+        implements /systems/{systemId}
+
+        @type system_id: string
+        @param system_id: id of system
+        @type data: string
+        @param data: system data
+
+        @returns: `dict` of system metadata
+        """
+
+        path = f'systems/{system_id}'
+        return self._request(path=path, method='POST', data=data)
+
+    def system_update(self, system_id: str, data: str) -> dict:
+        """
+        implements /systems/{systemId}
+
+        @type system_id: string
+        @param system_id: id of system
+        @type data: string
+        @param data: system data
+
+        @returns: `dict` of system metadata
+        """
+
+        path = f'systems/{system_id}'
+        return self._request(path=path, method='PUT', data=data)
+
+    def system_delete(self, system_id: str) -> bool:
+        """
+        implements /systems/{systemId}
+
+        @type system_id: string
+        @param system_id: id of system
+
+        @returns: `bool` of deletion result
+        """
+
+        path = f'systems/{system_id}'
+        return self._request(path=path, method='DELETE')
+
+    def system_components(self, system_id: str) -> dict:
+        """
+        implements /systems/{systemId}/components
+
+        @type system_id: string
+        @param system_id: id of system
+
+        @returns: `dict` of system components
+        """
+
+        path = f'systems/{system_id}/components'
+        return self._request(path=path)
+
+    def system_components_create(self, system_id: str, data: str) -> dict:
+        """
+        implements POST /systems/{systemId}/components
+
+        @type system_id: string
+        @param system_id: id of system
+        @type data: string
+        @param data: raw representation of data
+
+        @returns: single component result
+        """
+
+        path = f'systems/{system_id}/components'
+        return self._request(method='POST', path=path, data=data)
+
+    def system_deployments(self, system_id: str) -> dict:
+        """
+        implements /systems/{systemId}/deployments
+
+        @type system_id: string
+        @param system_id: id of system
+
+        @returns: `dict` of system deployments
+        """
+
+        path = f'systems/{system_id}/deployments'
+        return self._request(path=path)
+
+    def system_sampling_features(self, system_id: str) -> dict:
+        """
+        implements /systems/{systemId}/samplingFeatures
+
+        @type system_id: string
+        @param system_id: id of system
+
+        @returns: `dict` of system sampling features
+        """
+
+        path = f'systems/{system_id}/samplingFeatures'
+        return self._request(path=path)
